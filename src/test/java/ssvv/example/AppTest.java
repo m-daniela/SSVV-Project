d16c39cb79f92829971ef8bf7f4c78ab6e82f684
Nenisca Maria <neniscamaria13@gmail.com>
2021-03-09 15:42:28 +0200

    test add student with invalid name

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,8 +1,14 @@
 package ssvv.example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import ssvv.example.domain.Student;
+import ssvv.example.repository.NotaXMLRepo;
+import ssvv.example.repository.StudentXMLRepo;
+import ssvv.example.repository.TemaXMLRepo;
+import ssvv.example.service.Service;
+import ssvv.example.validation.NotaValidator;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
 
 /**
  * Unit test for simple App.
@@ -12,9 +18,55 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    Service service;
+
+    private void initialize(){
+        if(service==null) {
+            StudentValidator studentValidator = new StudentValidator();
+            TemaValidator temaValidator = new TemaValidator();
+            String filenameStudent = "src/test/java/ssvv/example/fisiere/Studenti.xml";
+            String filenameTema = "src/test/java/ssvv/example/fisiere/Teme.xml";
+            String filenameNota = "src/test/java/ssvv/example/fisiere/Note.xml";
+
+            StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+            TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+            NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+            NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+            service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        }
     }
+
+    private Student getValidStudent(){
+        return new Student("id1","John",935,"john@gmail.com");
+    }
+
+    private Student getStudentWithEmptyName(){
+        return new Student("id2","",935,"john@gmail.com");
+    }
+    @Test
+    public void addStudentWithValidName_ShouldReturnTrue()
+    {
+        initialize();
+        Student student = getValidStudent();
+        try{
+            Student addedStudent = service.addStudent(student);
+            assert(addedStudent == student);
+        }catch (Exception ex){
+            assert(false);
+        }
+    }
+
+    @Test
+    public void addStudentWithInvalidName_ShouldReturnFalse()
+    {
+        initialize();
+        Student student = getStudentWithEmptyName();
+        try{
+            service.addStudent(student);
+            assert(false);
+        }catch (Exception ex){
+            assert(true);
+        }
+    }
+
 }
